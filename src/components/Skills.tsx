0f8c965c0a129e3c55a244e165761ed8143e06ea
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-27 01:10:04 -0300

    Atualizar estilos e adicionar negrito nas habilidades

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Skills: React.FC = () => {
     return (
         <div className="w-full h-full py-24 flex flex-col justify-center items-center gap-6">
-            <div className="flex flex-row justify-center items-center gap-14 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-40 flex-wrap">
-                <div className="text-xl">🌩️ Pytorch</div>
-                <div className="text-xl">🖥️ NextJs</div>
-                <div className="text-xl">💨 FastAPI</div>
-                <div className="text-xl">🎲 PostgreSQL</div>
+            <div className="flex flex-row justify-center items-center gap-14 sm:gap-8 md:gap-40 lg:gap-40 xl:gap-40 flex-wrap">
+                <div className="text-xl font-bold">🌩️ Pytorch</div>
+                <div className="text-xl font-bold">🖥️ NextJs</div>
+                <div className="text-xl font-bold">💨 FastAPI</div>
+                <div className="text-xl font-bold">🎲 PostgreSQL</div>
             </div>
-            <div className="flex flex-row justify-center items-center gap-14 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-40 flex-wrap">
-                <div className="text-xl">🧠 Deep Learning</div>
-                <div className="text-xl">🌍 Foundation Models</div>
-                <div className="text-xl">👀 Computer Vision</div>
+            <div className="flex flex-row justify-center items-center gap-14 sm:gap-8 md:gap-40 lg:gap-40 xl:gap-40 flex-wrap">
+                <div className="text-xl font-bold">🧠 Deep Learning</div>
+                <div className="text-xl font-bold">🌍 Foundation Models</div>
+                <div className="text-xl font-bold">👀 Computer Vision</div>
             </div>
         </div>
 
