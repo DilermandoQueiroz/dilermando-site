7e060a77526084cac16d5fa52399217bff11eb3b
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-27 19:43:45 -0300

    Adiciona estilos compartilhados e bibliotecas de terceiros

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,10 @@
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-tomorrow.css'
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
