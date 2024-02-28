96b3b5db7b02f1d000a0c93772b0a9c6d93bb19d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-27 23:00:21 -0300

    Remover imagem de publicação não utilizada

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -12,7 +12,7 @@ const Publication: React.FC<PublicationProps> = ({ title, date, id }) => {
         <div className="flex justify-center items-center w-full">
             <Link href={`/blog/${id}`}>
             <div className="max-w-5xl p-5 border-b border-black flex flex-col justify-center items-center">
-                <img className="max-w-full h-auto" src="https://via.placeholder.com/732x564" alt="Publication" />
+                {/* <img className="max-w-full h-auto" src="https://via.placeholder.com/732x564" alt="Publication" /> */}
                 <div className="w-full mt-5">
                     <div className="text-black text-3xl font-semibold">{title}</div>
                     <div className="text-black text-base font-normal mt-1">{date}</div>
