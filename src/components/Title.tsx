674f91d791172b7a9367438fd36de15793438c5e
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-05-28 12:59:57 -0300

    pb-12

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,11 +10,11 @@ interface TitleProps {
 const Title: React.FC<TitleProps> = ({ title, authors, date, paperLink }) => {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full py-24 sm:py-36 px-2">
-            <div className="text-center text-4xl font-bold max-w-5xl">{title}</div>
-            <div className="text-center pt-12 max-w-2xl">{authors}</div>
-            <div className="text-center pt-2">{date}</div>
+            <div className="text-center pb-6">{date}</div>
+            <div className="text-center text-4xl font-bold max-w-5xl pb-6">{title}</div>
+            <div className="text-center pb-12 max-w-2xl">{authors}</div>
             {paperLink && (
-                <div className="text-center pt-12">
+                <div className="text-center">
                     <a href={paperLink}>
                         <div className="w-48 h-14 px-14 py-3.5 rounded-3xl border border-black justify-center items-center inline-flex hover:text-white hover:bg-black transition-all duration-300">
                             <div className="text-xl">Paper</div>
