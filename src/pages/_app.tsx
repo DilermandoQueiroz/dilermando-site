798e1a609aae7674e4550b61ac8a228ba730e6fd
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-09-06 11:54:42 -0300

    Update next.js version and add @vercel/analytics dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,13 @@ import 'katex/dist/katex.min.css'
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className='mb-12'>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
