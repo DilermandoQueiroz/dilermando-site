cbf28fe65bd5e87ea6c5331ea2e418113afe1f4e
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2025-01-25 20:16:06 -0300

    Atualiza a descrição no componente 'Name' de 'Machine learning' para 'AI Ethics'

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -5,7 +5,7 @@ const Name: React.FC = () => {
         <div className="w-full h-full flex justify-center items-center">
             <div className="text-center">
                 <div className="text-5xl font-extrabold">Dilermando Queiroz Neto</div>
-                <div className="text-neutral-500 text-2xl py-4 font-normal">Machine learning</div>
+                <div className="text-neutral-500 text-2xl py-4 font-normal">AI Ethics</div>
             </div>
         </div>
 
