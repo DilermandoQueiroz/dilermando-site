76589904a20fae1ab2e87bd81e302fe85211c223
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2025-02-21 19:00:47 -0300

    Ajusta o layout do componente 'Publication' para melhorar a exibição da imagem e do título

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -13,10 +13,16 @@ const Publication: React.FC<PublicationProps> = ({ title, date, id, image}) => {
         <div className="flex justify-center items-center w-full p-5">
             <Link href={`/blog/${id}`}>
                 <div className="max-w-5xl border-b border-black flex flex-col items-center">
-                    <div className="flex justify-center items-center">
-                        <img className="w-86 h-86 rounded-xl" src={image.toString()} alt="Publication" /> 
+                    <div className="flex justify-center items-center w-full">
+                        <div className="w-[20rem] md:w-[35rem] h-96 overflow-hidden rounded-xl">
+                            <img 
+                                className="w-full h-full object-cover" 
+                                src={image.toString()} 
+                                alt={title} 
+                            /> 
+                        </div>
                     </div>
-                    <div className="py-4 py-4 w-[20rem] md:w-[35rem]">
+                    <div className="py-4 w-[20rem] md:w-[35rem]">
                         <div className="text-3xl font-semibold">{title}</div>
                         <div className="text-base font-normal">{date}</div>
                     </div>
